29900541fd12d27ddd1ed2528b695d5845be8905
cinnamew <101888513+cinnamew@users.noreply.github.com>
2023-10-10 10:46:24 -0700

    favicon doesn't work :/ [some more game dev stuff]

--- js/gamedev.js
+++ js/gamedev.js
@@ -1,4 +1,4 @@
-document.getElementById("cards").onmousemove = e => {
+document.getElementById("test").onmousemove = e => {
     for(const card of document.getElementsByClassName("card")) {
       const rect = card.getBoundingClientRect(),
             x = e.clientX - rect.left,
